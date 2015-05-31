eb1978613e7b250a4f65a45bb22c7a65638b5aa9
Roland Rabien <figbug@gmail.com>
2015-05-31 12:05:15 -0700

    Build for 64 bits

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 # Quick hack to build libarc.a suitable for both simulator and device.
 
-SDK_VERSION=6.0
+SDK_VERSION=8.3
 
 cd arc
-xcodebuild -sdk iphoneos$SDK_VERSION "ARCHS=armv6 armv7 armv7s" clean build
+xcodebuild -sdk iphoneos$SDK_VERSION "ARCHS=arm64 armv7 armv7s" clean build
 xcodebuild -sdk iphonesimulator$SDK_VERSION "ARCHS=i386 x86_64" "VALID_ARCHS=i386 x86_64" clean build
 lipo -output build/libarc.a -create build/Release-iphoneos/libarc.a build/Release-iphonesimulator/libarc.a
